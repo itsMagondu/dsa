513712fcbda00510891422bc949dfff0dada4d72
Samuel Magondu <magondunjenga@gmail.com>
2021-04-05 12:30:08 +0300

    Add matrix multop;ocation code

---
+++ inneficient_matrix_multipy.py
@@ -0,0 +1,14 @@
+x = [[1,2,3],[3,4,5]]
+y = [[4,5],[6,7],[8,9]]
+
+## Add validation step that x row size == y columns.
+n = len(x)
+first_row_len = len(x[0])  # Allows us to multiply matrices of any length.
+c = [[0]*n for i in range(n)]
+
+for i in range(n):
+  for j in range(n):
+    for k in range(first_row_len):
+      c[i][j] = c[i][j] + x[i][k]*y[k][j]
+  
+print (c)
